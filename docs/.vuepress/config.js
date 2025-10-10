a0d2a8e6f97478bc4a27fdf87c2d723b062a97d9
cgy <307191020@qq.com>
2025-10-10 16:51:01 +0800

    add GitHub Actions config for deployment

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,4 +5,7 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
   bundler: viteBundler(),
   theme: defaultTheme(),
+    // 根据仓库地址设置 base
+  base: '/', // 仓库是 <username>.github.io 时用这个
+  // 如果是其他仓库，例如仓库名是 my-docs，则改为 base: '/my-docs/'
 })
