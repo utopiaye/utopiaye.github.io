9d4f73c0b0d5be92c694aa7e21e94d2080fc561b
cgy <307191020@qq.com>
2025-10-11 10:26:18 +0800

    docs: 更新配置文件中的客户端信息

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,10 @@
 const path = require('path')
+require('dotenv').config()
 
 module.exports = {
-  title: 'vuepress-theme-meteorlxy',
+  title: 'Metal-Organic-Framework',
 
-  description: 'Vuepress theme of meteorlxy\'s blog',
+  description: "MOF INFO. of cgy's blog",
 
   locales: {
     '/': {
@@ -14,9 +15,12 @@ module.exports = {
   evergreen: true,
 
   plugins: [
-    ['@vuepress/google-analytics', {
-      ga: 'UA-132770851-2',
-    }],
+    [
+      '@vuepress/google-analytics',
+      {
+        ga: 'UA-132770851-2',
+      },
+    ],
   ],
 
   chainWebpack: (config, isServer) => {
@@ -46,74 +50,16 @@ module.exports = {
     lang: 'en-US',
 
     personalInfo: {
-      nickname: 'meteorlxy',
-      description: 'Happy Coding<br/>Happy Life',
-      email: 'meteor.lxy@foxmail.com',
-      location: 'Shenzhen, China',
+      nickname: 'cgy',
+      description: 'MOF',
+      email: '307191020@qq.com',
+      location: 'gz, China',
       organization: 'Tencent',
-
       avatar: 'https://www.meteorlxy.cn/assets/img/avatar.jpg',
-
       sns: {
         github: {
-          account: 'meteorlxy',
-          link: 'https://github.com/meteorlxy',
-        },
-        facebook: {
-          account: 'meteorlxy.cn',
-          link: 'https://www.facebook.com/meteorlxy.cn',
-        },
-        linkedin: {
-          account: 'meteorlxy',
-          link: 'http://www.linkedin.com/in/meteorlxy',
-        },
-        twitter: {
-          account: 'meteorlxy_cn',
-          link: 'https://twitter.com/meteorlxy_cn',
-        },
-        weibo: {
-          account: '@焦炭君_Meteor',
-          link: 'https://weibo.com/u/2039655434',
-        },
-        zhihu: {
-          account: 'meteorlxy.cn',
-          link: 'https://www.zhihu.com/people/meteorlxy.cn',
-        },
-        douban: {
-          account: '159342708',
-          link: 'https://www.douban.com/people/159342708',
-        },
-        reddit: {
-          account: 'meteorlxy',
-          link: 'https://www.reddit.com/user/meteorlxy',
-        },
-        medium: {
-          account: 'meteorlxy.cn',
-          link: 'https://medium.com/@meteorlxy.cn',
-        },
-        instagram: {
-          account: 'meteorlxy.cn',
-          link: 'https://www.instagram.com/meteorlxy.cn',
-        },
-        gitlab: {
-          account: 'meteorlxy',
-          link: 'https://gitlab.com/meteorlxy',
-        },
-        bitbucket: {
-          account: 'meteorlxy',
-          link: 'https://bitbucket.org/meteorlxy',
-        },
-        docker: {
-          account: 'meteorlxy',
-          link: 'https://hub.docker.com/u/meteorlxy',
-        },
-        csdn: {
-          account: '',
-          link: 'https://www.csdn.net/',
-        },
-        juejin: {
-          account: 'meteorlxy',
-          link: 'https://juejin.im/user/5c6fa9dde51d453fcb7baf09',
+          account: 'cgy',
+          link: 'https://github.com/utopiaye',
         },
       },
     },
@@ -129,7 +75,8 @@ module.exports = {
     footer: {
       poweredBy: true,
       poweredByTheme: true,
-      custom: 'Copyright 2018-present <a href="https://github.com/meteorlxy" target="_blank">meteorlxy</a> | MIT License',
+      custom:
+        'Copyright 2018-present <a href="https://github.com/utopiaye" target="_blank">utopiaye</a> | MIT License',
     },
 
     infoCard: {
@@ -145,8 +92,6 @@ module.exports = {
       { text: 'Home', link: '/', exact: true },
       { text: 'Posts', link: '/posts/', exact: false },
       { text: 'Custom Pages', link: '/custom-pages/', exact: false },
-      { text: 'Changelog', link: 'https://github.com/meteorlxy/vuepress-theme-meteorlxy/blob/master/CHANGELOG.md' },
-      { text: 'Github', link: 'https://github.com/meteorlxy/vuepress-theme-meteorlxy' },
     ],
 
     // Enable smooth scrolling or not
@@ -158,10 +103,10 @@ module.exports = {
     },
 
     comments: {
-      owner: 'meteorlxy',
-      repo: 'vuepress-theme-meteorlxy',
-      clientId: 'cbda894952ba70c00666',
-      clientSecret: '1ade785ca693bf3092be5e5338720d5ee43672b7',
+      owner: 'cgy',
+      repo: 'Metal-Organic-Framework',
+      clientId: process.env.VUEPRESS_COMMENT_CLIENT_ID,
+      clientSecret: process.env.VUEPRESS_COMMENT_CLIENT_SECRET,
       autoCreateIssue: false,
     },
 
