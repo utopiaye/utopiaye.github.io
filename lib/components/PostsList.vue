fb6d7014959cb01dcd77412d2f8a0fed65b32a5e
cgy <307191020@qq.com>
2025-10-11 16:29:30 +0800

    docs: 更新文件

--- lib/components/PostsList.vue
+++ lib/components/PostsList.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- 模板部分保持不变 -->
   <div class="main-div posts-list">
     <TransitionFadeSlide>
       <div
@@ -8,7 +9,6 @@
       >
         {{ $themeConfig.lang.noRelatedPosts }}
       </div>
-
       <div
         v-else
         :key="page"
@@ -28,7 +28,6 @@
         </TransitionFadeSlide>
       </div>
     </TransitionFadeSlide>
-
     <div
       v-if="total > 1"
       class="posts-paginator"
@@ -48,47 +47,36 @@ import Pagination from '@theme/components/Pagination.vue'
 
 export default {
   name: 'PostsList',
-
-  components: {
-    TransitionFadeSlide,
-    PostsListItem,
-    Pagination,
-  },
-
-  props: {
-    posts: {
-      type: Array,
-      required: false,
-      default: null,
-    },
-  },
-
+  components: { TransitionFadeSlide, PostsListItem, Pagination },
+  props: { posts: { type: Array, required: false, default: null } },
   data () {
-    return {
-      page: 1,
-    }
+    return { page: 1 }
   },
-
   computed: {
     perPage () {
       return this.$themeConfig.pagination.perPage || 5
     },
-
     total () {
       return Math.ceil(this.listPosts.length / this.perPage)
     },
-
     listPosts () {
-      return this.posts || this.$posts
+      const posts = this.posts || this.$posts
+      // 适配时分秒格式：正则提取 YYYY-MM-DD-HH-mm-ss
+      return posts.sort((a, b) => {
+        const timeA = a.path.match(/(\d{4}-\d{2}-\d{2}-\d{2}-\d{2}-\d{2})/)[0]
+        const timeB = b.path.match(/(\d{4}-\d{2}-\d{2}-\d{2}-\d{2}-\d{2})/)[0]
+        return (
+          new Date(timeB.replace(/-/g, ' ')) -
+          new Date(timeA.replace(/-/g, ' '))
+        )
+      })
     },
-
     pagePosts () {
       const begin = (this.page - 1) * this.perPage
       const end = begin + this.perPage
       return this.listPosts.slice(begin, end)
     },
   },
-
   watch: {
     listPosts () {
       this.page = 1
@@ -99,7 +87,5 @@ export default {
 
 <style lang="stylus" scoped>
 @require '~@theme/styles/variables'
-
-.no-posts
-  color $grayTextColor
+.no-posts { color $grayTextColor }
 </style>
